e93c80ba8236d8ed2b1be0adba2a28fae84864bb
Aukau <77255406+Aukau@users.noreply.github.com>
2021-09-27 11:50:07 -0700

    Forgot to properly address

--- hello.py
+++ hello.py
@@ -4,3 +4,7 @@
 #Hello World!
 
 print("Hello World!") #prints Hello World!
+
+#homework section of the file
+
+print("My name is Zac (He/Him). If you want to be more formal, you can use Zachary.") #Prints out my name, pronouns, and formalities
