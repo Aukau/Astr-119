8bf0d89c40b95b39f2b1200a3ea2ddc03446dcab
Aukau <77255406+Aukau@users.noreply.github.com>
2021-09-30 15:45:51 -0700

    Delete Astr-119-Hw-2 directory

--- Astr-119-Hw-2/getting_help.py
+++
@@ -1,3 +0,0 @@
-import os
-
-help(os)        #this will print the commands or information around the os stuff
--- Astr-119-Hw-2/python_use_module.py
+++
@@ -1,3 +0,0 @@
-import test_module as tm        #test_module.py has to be in the same folder(directory) as this program?
-
-tm.hello_world()        #yay it works!!!
