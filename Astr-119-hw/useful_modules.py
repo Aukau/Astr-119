7b41b72f87d0a27b4ac969421ba3c901122cabe2
Aukau <77255406+Aukau@users.noreply.github.com>
2021-10-05 13:35:51 -0700

    Delete Astr-119-hw directory

--- Astr-119-hw/useful_modules.py
+++
@@ -1,8 +0,0 @@
-import numpy as np      #the numpy library
-import matplotlib.pyplot as plt     #this is matplotlib's pyplot
-
-import sys  #gives access to C-like sys libraries
-import os   #gives access to operating systems
-
-print(sys.argv)     #command line arguments
-print(os.getcwd)    #this gives the current working directory
